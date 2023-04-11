12abd54cff924b2cf264562debcbb711b3470926
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-04-11 16:23:35 +0300

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Card.css';
 
 const Card = (props) => {
-  return <div className='card'>{props.children}</div>;
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Card;
