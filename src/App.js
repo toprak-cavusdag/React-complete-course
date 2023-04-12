113de2ccbffe9b148b74e633929d011a745cc37d
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-04-12 11:36:03 +0300

    sorting by date is ready

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/expenses/Expenses';
+import React, { useState } from 'react';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toliet Paper',
+    amount: 94.12,
+    date: new Date(2023, 2, 28),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 789.99,
+    date: new Date(2023, 1, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 296.65,
+    date: new Date(2023, 9, 1),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 545.91,
+    date: new Date(2022, 12, 30),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toliet Paper',
-      amount: 94.12,
-      date: new Date(2023, 2, 28),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 789.99,
-      date: new Date(2023, 1, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 296.65,
-      date: new Date(2023, 9, 1),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 545.91,
-      date: new Date(2022, 12, 30),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
   };
 
   return (
--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -5,20 +5,13 @@ import ExpenceDate from './ExpenceDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Toprak');
-    console.log(title);
-  };
   return (
     <Card className='expense-item'>
       <ExpenceDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
